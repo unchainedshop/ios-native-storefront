5634dc0b45f5787589381534457904752db34bf0
Pascal Kaufmann <kaufmae@me.com>
2020-04-13 12:47:16 +0200

    Initial Commit

---
+++ ios-native-storefront/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-native-storefront
+//
+//  Created by Pascal Kaufmann on 13.04.20.
+//  Copyright © 2020 Pascal Kaufmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
